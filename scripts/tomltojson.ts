3f1c2e440cc33c2bd2a308cef2a679eac1c8efde
lupomikti <25500382+lupomikti@users.noreply.github.com>
2026-06-16 07:12:18 -0700

    feat(vsc)!: refactor and improve syntaxes

--- scripts/tomltojson.ts
+++ scripts/tomltojson.ts
@@ -1,10 +1,19 @@
 #!/usr/bin/env -S deno run
-import TOML from 'smol-toml'
-import { readFileSync, writeFileSync } from 'node:fs'
+import TOML from "smol-toml";
+import { readFileSync, writeFileSync } from "node:fs";
 
-const inputFiles = ["migoto.tmLanguage.toml", "regexp.tmLanguage.toml", "regexp-replace.tmLanguage.toml"]
+const inputFiles = [
+  "migoto.tmLanguage.toml",
+  "regexp.tmLanguage.toml",
+  "regexp-replace.tmLanguage.toml",
+];
 
 for (const inputFile of inputFiles) {
-    const obj = TOML.parse(readFileSync(`toml-syntaxes/${inputFile}`, {encoding: 'utf-8'}))
-    writeFileSync(`vscode-ext/syntaxes/${inputFile.replace(/\.toml/, ".json")}`, JSON.stringify(obj, null, 2))
+  const obj = TOML.parse(
+    readFileSync(`toml-syntaxes/${inputFile}`, { encoding: "utf-8" }),
+  );
+  writeFileSync(
+    `vscode-ext/${inputFile.replace(/\.toml/, ".json")}`,
+    JSON.stringify(obj, null, 2),
+  );
 }
