6d0bc184e915166a87d2a19a92c3f871c0f46f51
lupomikti <25500382+lupomikti@users.noreply.github.com>
2026-06-16 07:13:40 -0700

    feat(sublime)!: refactor and improve syntaxes

--- scripts/tomltoyaml.ts
+++ scripts/tomltoyaml.ts
@@ -1,14 +1,24 @@
 #!/usr/bin/env -S deno run
-import TOML from 'smol-toml'
-import YAML from 'yaml'
-import * as prettier from 'prettier'
-import { readFileSync, writeFileSync } from 'node:fs'
+import TOML from "smol-toml";
+import YAML from "yaml";
+import * as prettier from "prettier";
+import { readFileSync, writeFileSync } from "node:fs";
 
-const inputFiles = ["Migoto.sublime-syntax.toml"]
+const inputFiles = [
+  "Migoto.sublime-syntax.toml",
+  "PCRE2.sublime-syntax.toml",
+  "PCRE2-Replace.sublime-syntax.toml",
+];
 
 for (const inputFile of inputFiles) {
-    const inputFilePath = `toml-syntaxes/${inputFile}`
-    const obj = TOML.parse(readFileSync(inputFilePath, {encoding: 'utf-8'}))
-    const prettyResult = await prettier.format(`%YAML 1.2\n---\n${YAML.stringify(obj, {defaultKeyType: 'PLAIN'})}`, { parser: 'yaml' })
-    writeFileSync(`sublime-text-pkg/${inputFile.replace(/\.toml/, "")}`, prettyResult)
+  const inputFilePath = `toml-syntaxes/${inputFile}`;
+  const obj = TOML.parse(readFileSync(inputFilePath, { encoding: "utf-8" }));
+  const prettyResult = await prettier.format(
+    `%YAML 1.2\n---\n${YAML.stringify(obj, { defaultKeyType: "PLAIN" })}`,
+    { parser: "yaml" },
+  );
+  writeFileSync(
+    `sublime-text-pkg/${inputFile.replace(/\.toml/, "")}`,
+    prettyResult,
+  );
 }
